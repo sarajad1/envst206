440f1d92e9da3240e6db08ad5a89d3fc76b36775
Sara Jadbabaie <sara@saras-air-2.hamilton.edu>
2020-09-02 17:20:52 -0400

    added new calculations

--- activity 1.R
+++ activity 1.R
@@ -2,3 +2,15 @@
 print("hello R world")
 
 2*2
+#4 plus 5 raised to the second power
+(4+5)^2
+#name my number
+x <- 81
+x/9
+x^2 
+#making a vector with four numbers
+b <- c(4, 10, 12, 16)
+#multiple vector by 5
+b*5
+a <- c(10, 20, 30, 40)
+a*10/2
